7f8e32e51720286484fc04feecc4b7d2451f524f
Tom Fitzpatrick <tom@tomfitzpatrick.net>
2022-12-18 14:57:26 -0600

    direction model implemented

--- src/main/java/co/weathertrail/weathertrail/models/Direction.java
+++ src/main/java/co/weathertrail/weathertrail/models/Direction.java
@@ -1,4 +1,12 @@
 package co.weathertrail.weathertrail.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+// TODO figure out why geocoded_waypoints in DirectionResponse exist
 public class Direction {
+    private List<Route> routes = new ArrayList<>();
+    private String directionStatus;
+    private List<String> availableTravelModes = new ArrayList<>();
+    private String errorMessage;
 }
