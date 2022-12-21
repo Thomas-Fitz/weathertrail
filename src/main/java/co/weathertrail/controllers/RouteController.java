cc86cd156924f34d70b5919c8197b177524806ed
Tom Fitzpatrick <tom@tomfitzpatrick.net>
2022-12-20 22:16:52 -0600

    notes on milestone controller

--- src/main/java/co/weathertrail/controllers/RouteController.java
+++ src/main/java/co/weathertrail/controllers/RouteController.java
@@ -32,4 +32,5 @@ public class RouteController {
         return response;
     }
 
+
 }
---
+++ src/main/java/co/weathertrail/models/Milestone.java
@@ -0,0 +1,9 @@
+package co.weathertrail.models;
+
+public class Milestone {
+    // step
+    // location
+    // time (duration)
+    // distance (meters)
+    // time (unix)
+}
